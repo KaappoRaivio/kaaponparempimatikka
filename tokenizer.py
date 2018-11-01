f58862541d48883c6452be8b071f1adb99373a67
NoSocksForYou <kaappo.raivio@gmail.com>
2018-11-01 13:47:43 +0200

    autocommit

--- tokenizer.py
+++ tokenizer.py
@@ -1,8 +1,11 @@
 import re
-from typing import Dict, Any
+from typing import Dict, Any, Iterable
 
 from _operatorD import builtins
 
+
+OPENING_BRACE = 5
+CLOSING_BRACE = 6
 
 class Tokenizer:
     def __init__(self, tokens: Dict[str, Any]):
@@ -11,18 +14,34 @@ class Tokenizer:
     def getRegex(self):
         return re.compile(r"|".join(list(map(lambda x: f"\{x}", self.tokens))))
 
+
+    @staticmethod
+    def regexIter(iterable: Iterable[str]):
+        for i in iterable:
+            yield re.compile(re.escape(i)), i
+
+    @staticmethod
+    def smartIter(end, already_itered=()):
+        for i in range(end):
+            if i not in already_itered:
+                yield i
+            else:
+                continue
+
     def tokenize(self, expr: str):
-        for token in self.tokens:
-            print(f"token: {token}")
+        already_itered = ()
+
+        for regex, token in self.regexIter(sorted(TOKENS, key=len, reverse=True)): #searching for the longest tokens first
+
             slice_len = len(token)
-            # print(len(expr) - slice_len)
-            for a in range(len(expr) - slice_len):
+
+            for a in self.smartIter(len(expr) - slice_len, already_itered):
                 slice = expr[a:a + slice_len]
-                # print(slice)
                 try:
                     value = self.tokens[slice]
                     if value == self.tokens[token]:
                         print(f"Found: value: {value}, token: {token}, from {slice}, index: {a}")
+                        already_itered += tuple(range(a, a + slice_len))
                 except KeyError:
                     continue
 
@@ -31,12 +50,13 @@ TOKENS = {
     "+": builtins.OPERATOR_BINARY_ADD,
     "-": builtins.OPERATOR_BINARY_SUBTRACT,
     "**": builtins.OPERATOR_BINARY_POWER,
-    "/": builtins.OPERATOR_BINARY_DIVIDE,
     "*": builtins.OPERATOR_BINARY_MULTIPLY,
+    "/": builtins.OPERATOR_BINARY_DIVIDE,
     "(": 5,
     ")": 6
 }
 
-TESTEXPR = "(3 + 2) * 3"
+
+TESTEXPR = "(3 + 2) ** 3"
 
 Tokenizer(TOKENS).tokenize(TESTEXPR)
